2f65f8fc2e642c8333c95c967c193bdeabd69764
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2024-03-26 19:13:18 +0800

    Upload message board

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Form'});
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.author, added: new Date()});
+  res.redirect('/');
+});
+
+module.exports = router;
